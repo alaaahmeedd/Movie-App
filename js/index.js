a31bfc926fd43b27d3b2295e474bd9dae6e4db93
alaaahmeedd <alaa.aahmeedd@gmail.com>
2023-09-10 00:54:45 +0300

    editing on api function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
 let rowData = document.getElementById('rowData');
+let arr_movies = [];
 
 function openSideNav(){
     $('.side-nav-menu').animate({ left:0 },500);
@@ -35,39 +36,29 @@ $('.side-nav-menu i.open-close-icon').click(()=>{
     }
 })
 
-async function search(term){
-    let response = await fetch('https://api.themoviedb.org/3/search/movie?api_key=1678a819276e7e59cbbc5082405f88bb&language=en-US&query=${term}&page=1&include_adult=false')
-    let finalResult = await response.json()
-    arr_movies = finalResult.results;
-    displayMovie()
 
-
-}
 
 //  --------------- Api ----------------
 
-function displayMovie(){
+function displayMovies(){
     let cartoona = '';
     
     for( let i=0; i<arr_movies.length ; i++ ){
         cartoona += `
         <div class="col-md-4">
                 <div class="movie-item position-relative overflow-hidden">
-                    <img src="https://image.tmdb.org/t/p/w500/${arr_movies[i].poster_path}}" class="w-100 animate">
-                    <div class="movie-overlay position-absolute p-5">
-                        <h1 class="movie-title"> Meg 2: The Trench </h1>
-                        <p class="movie-desc"> An exploratory dive into the deepest depths of the ocean
-                            of a daring research team spirals into chaos when a malevolent
-                            mining operation threatens their mission and forces them into
-                            a high-stakes battle for survival. </p>
-                        <p class="movie-date"> <span> Release Date </span> <span>  : 2023-08-02 </span></p>
+                    <img src="https://image.tmdb.org/t/p/w500/${arr_movies[i].poster_path}" class="w-100 animate">
+                    <div class="movie-overlay position-absolute p-5 d-flex flex-column justify-content-center">
+                        <h1 class="movie-title"> ${arr_movies[i].title} </h1>
+                        <p class="movie-desc"> ${arr_movies[i].overview} </p>
+                        <p class="movie-date">  <span> Release Date </span> <span>  : ${arr_movies[i].release_date} </span></p>
                         <h3 class="">
                             <i class="fa-solid fa-star text-warning fs-6"></i>
                             <i class="fa-solid fa-star text-warning fs-6"></i>
                             <i class="fa-solid fa-star text-warning fs-6"></i>
                             <i class="fa-regular fa-star-half-stroke text-warning fs-6"></i>
                         </h3>
-                        <h3 class="number"> 7.0</h3>    
+                        <h3 class="number"> ${arr_movies[i].vote_average} </h3>    
 
                     </div>
                 </div>
@@ -77,99 +68,204 @@ function displayMovie(){
     rowData.innerHTML = cartoona
 }
 
-search()
+async function getApi(type = "popular") {
+    let response = await fetch(
+      `https://api.themoviedb.org/3/movie/${type}?api_key=eba8b9a7199efdcb0ca1f96879b83c44&fbclid=IwAR32Px4_3ZTHYF-tjdSOdkN82Esd5XSCl7c0ueF0LR8urOnlJBZ4TJJdf_k`
+    );
 
-
-
-
-
-
-///////////////////// contact /////////////////////
-
-// function validateContactForm() {
-//     const nameInput = document.querySelector('input[placeholder="Enter Your Name"]');
-//     const emailInput = document.querySelector('input[placeholder="Enter Your Email"]');
-//     const phoneInput = document.querySelector('input[placeholder="Enter Your Phone"]');
-//     const ageInput = document.querySelector('input[placeholder="Enter Your Age"]');
-//     const passwordInput = document.querySelector('input[placeholder="Enter Your Password"]');
-//     const rePasswordInput = document.querySelector('input[placeholder="Re-enter Password"]');
+    let finalResult = await response.json();
+    arr_movies = finalResult.results;
+    displayMovies();
+  }
   
-//     const userNameAlert = document.getElementById('userNameAlert');
-//     const userEmailAlert = document.getElementById('userEmailAlert');
-//     const userPhoneAlert = document.getElementById('userPhoneAlert');
-//     const userAgeAlert = document.getElementById('userAgeAlert');
-//     const userPasswordAlert = document.getElementById('userPasswordAlert');
-//     const userRePasswordAlert = document.getElementById('userRePasswordAlert');
+  async function getTrending() {
+    let response = await fetch(
+      "https://api.themoviedb.org/3/trending/all/week?api_key=1678a819276e7e59cbbc5082405f88bb"
+    );
+    let finalResult = await response.json();
+    arr_movies = finalResult.results;
+    displayMovies();
+  }
+  getApi();
   
-//     // Regular expressions for validation
-//     const nameRegex = /^[A-Za-z]{3,}$/;
-//     const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-//     const phoneRegex = /^\d{10}$/;
-//     const ageRegex = /^(1[6-9]|[2-9]\d)$/;
-//     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-  
-//     // Validate name field
 
-//     function userNameValidation(){
-       
-//         if(nameRegex.test(userNameInput.value) == true && userNameInput.value != '' ){
-//             userNameInput.classList.add('is-valid');
-//             userNameInput.classList.remove('is-invalid');
-//             userNameAlert.classList.replace('d-block' , 'd-none');
-//             return true;
-//         }else{
-//             userNameInput.classList.add('is-invalid');
-//             userNameInput.classList.remove('is-valid');
-//             userNameAlert.classList.replace('d-none' , 'd-block');
-//             return false;
-//         }
-    
+
+
+  $("#nowPlaying").click(() => {
+    getApi("now_playing");
+  });
+  
+  $("#popular").click(() => {
+    getApi("popular");
+  });
+  
+  $("#topRated").click(() => {
+    getApi("top_rated");
+  });
+  
+  $("#trending").click(() => {
+    getTrending();
+  });
+  
+  $("#upcoming").click(() => {
+    getApi("upcoming");
+  });
+
+
+// ---------------------- search ---------------------
+
+/* ------------------ search bars ----------------- */
+
+let searchMovies = document.querySelector("#inputSearch");
+let searchCurrentList = document.querySelector("#currentSearch");
+
+async function searchAllMovies(term) {
+  let response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=1678a819276e7e59cbbc5082405f88bb&language=en-US&query=${term}&page=1&include_adult=false`
+  );
+
+  let finalResult = await response.json();
+  arr_movies = finalResult.results;
+  displayMovies();
+
+}
+
+$("#inputSearch").keyup(() => {
+  searchAllMovies(searchMovies.value);
+});
+
+/* ------------------ search current ----------------- */
+
+
+
+
+
+
+// ---------------------- contact ----------------------
+
+function validateContactForm() {
+    const userNameInput = document.getElementById('userNameInput');
+    const userEmailInput = document.getElementById('userEmailInput');
+    const userPhoneInput = document.getElementById('userPhoneInput');
+    const userAgeInput = document.getElementById('userAgeInput');
+    const userPasswordInput = document.getElementById('userPasswordInput');
+    const userRePasswordInput = document.getElementById('userRePasswordInput');
+  
+    const userNameAlert = document.getElementById('userNameAlert');
+    const userEmailAlert = document.getElementById('userEmailAlert');
+    const userPhoneAlert = document.getElementById('userPhoneAlert');
+    const userAgeAlert = document.getElementById('userAgeAlert');
+    const userPasswordAlert = document.getElementById('userPasswordAlert');
+    const userRePasswordAlert = document.getElementById('userRePasswordAlert');
+  
+    // Regular expressions for validation
+    const nameRegex = /^[A-Za-z]{3,}$/;
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+    const phoneRegex = /^\d{10}$/;
+    const ageRegex = /^(1[6-9]|[2-9]\d)$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  
+    // Validate name field
+    if (!nameRegex.test(userNameInput.value.trim())) {
+      userNameInput.classList.add('is-invalid');
+      userNameInput.classList.remove('is-valid');
+      userNameAlert.classList.remove('d-none');
+    } else {
+      userNameInput.classList.add('is-valid');
+      userNameInput.classList.remove('is-invalid');
+      userNameAlert.classList.add('d-none');
+    }
+  
+    // Validate email field
+    if (!emailRegex.test(userEmailInput.value.trim())) {
+      userEmailInput.classList.add('is-invalid');
+      userEmailInput.classList.remove('is-valid');
+      userEmailAlert.classList.remove('d-none');
+    } else {
+      userEmailInput.classList.add('is-valid');
+      userEmailInput.classList.remove('is-invalid');
+      userEmailAlert.classList.add('d-none');
+    }
+  
+    // Validate phone field
+    if (!phoneRegex.test(userPhoneInput.value.trim())) {
+      userPhoneInput.classList.add('is-invalid');
+      userPhoneInput.classList.remove('is-valid');
+      userPhoneAlert.classList.remove('d-none');
+    } else {
+      userPhoneInput.classList.add('is-valid');
+      userPhoneInput.classList.remove('is-invalid');
+      userPhoneAlert.classList.add('d-none');
+    }
+  
+    // Validate age field
+    if (!ageRegex.test(userAgeInput.value.trim())) {
+      userAgeInput.classList.add('is-invalid');
+      userAgeInput.classList.remove('is-valid');
+      userAgeAlert.classList.remove('d-none');
+    } else {
+      userAgeInput.classList.add('is-valid');
+      userAgeInput.classList.remove('is-invalid');
+      userAgeAlert.classList.add('d-none');
+    }
+  
+    // Validate password field
+    if (!passwordRegex.test(userPasswordInput.value.trim())) {
+      userPasswordInput.classList.add('is-invalid');
+      userPasswordInput.classList.remove('is-valid');
+      userPasswordAlert.classList.remove('d-none');
+    } else {
+      userPasswordInput.classList.add('is-valid');
+      userPasswordInput.classList.remove('is-invalid');
+      userPasswordAlert.classList.add('d-none');
+    }
+  
+    // Validate re-enter password field
+    if (userPasswordInput.value !== userRePasswordInput.value) {
+      userRePasswordInput.classList.add('invalid');
+      userRePasswordInput.classList.remove('is-invalid');
+      userRePasswordAlert.classList.remove('d-none');
+    } else {
+      userRePasswordInput.classList.add('is-invalid');
+      userRePasswordInput.classList.remove('is-valid');
+      userRePasswordAlert.classList.add('d-none');
+    }
+  }
+
+
+
+//   function searchCurrent(movieTitle) {
+//     let cartoona = '';
+
+//   for (let i = 0; i < arr_movies.length; i++) {
+//     if (arr_movies[i].title.includes(movieTitle) == true) {
+//         cartoona += `
+//         <div class="col-md-4">
+//                 <div class="movie-item position-relative overflow-hidden">
+//                     <img src="https://image.tmdb.org/t/p/w500/${arr_movies[i].poster_path}" class="w-100 animate">
+//                     <div class="movie-overlay position-absolute p-5 d-flex flex-column justify-content-center">
+//                         <h1 class="movie-title"> ${arr_movies[i].title} </h1>
+//                         <p class="movie-desc"> ${arr_movies[i].overview} </p>
+//                         <p class="movie-date">  <span> Release Date </span> <span>  : ${arr_movies[i].release_date} </span></p>
+//                         <h3 class="">
+//                             <i class="fa-solid fa-star text-warning fs-6"></i>
+//                             <i class="fa-solid fa-star text-warning fs-6"></i>
+//                             <i class="fa-solid fa-star text-warning fs-6"></i>
+//                             <i class="fa-regular fa-star-half-stroke text-warning fs-6"></i>
+//                         </h3>
+//                         <h3 class="number"> ${arr_movies[i].vote_average} </h3>    
+
+//                     </div>
+//                 </div>
+//             </div>
+//         `;
 //     }
-    
-  
-//     // Validate email field
-//     if (!emailRegex.test(emailInput.value)) {
-//       userEmailAlert.classList.remove('d-none');
-//       return false;
-//     } else {
-//       userEmailAlert.classList.add('d-none');
-//     }
-  
-//     // Validate phone field
-//     if (!phoneRegex.test(phoneInput.value)) {
-//       userPhoneAlert.classList.remove('d-none');
-//       return false;
-//     } else {
-//       userPhoneAlert.classList.add('d-none');
-//     }
-  
-//     // Validate age field
-//     if (!ageRegex.test(ageInput.value)) {
-//       userAgeAlert.classList.remove('d-none');
-//       return false;
-//     } else {
-//       userAgeAlert.classList.add('d-none');
-//     }
-  
-//     // Validate password field
-//     if (!passwordRegex.test(passwordInput.value)) {
-//       userPasswordAlert.classList.remove('dnone');
-//       return false;
-//     } else {
-//       userPasswordAlert.classList.add('d-none');
-//     }
-  
-//     // Validate re-enter password field
-//     if (passwordInput.value !== rePasswordInput.value) {
-//       userRePasswordAlert.classList.remove('d-none');
-//       return false;
-//     } else {
-//       userRePasswordAlert.classList.add('d-none');
-//     }
-  
-//     // All fields are valid
-//     return true;
+//     document.getElementById("movies_Data").innerHTML = cartoona;
 //   }
+// }
+
+
+
 
 
 
