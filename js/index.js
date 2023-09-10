f6730e9f48b59d5f75f472985c862aaa9a445c7f
alaaahmeedd <alaa.aahmeedd@gmail.com>
2023-09-11 00:14:16 +0300

    add icon back to top

--- js/index.js
+++ js/index.js
@@ -241,6 +241,24 @@ function validateContactForm() {
   }
 
 
+  window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+      document.getElementById("backToTopBtn").style.display = "none";
+    }
+  }
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+
 
 
 
